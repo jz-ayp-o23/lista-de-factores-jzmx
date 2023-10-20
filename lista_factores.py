5c503781ce73c62145bd356ceeb2f94b6dc26747
jzmx <jorge@zaldivar.mx>
2023-10-20 01:31:21 +0000

    Code solution, first try

--- lista_factores.py
+++ lista_factores.py
@@ -1,15 +1,18 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Lista de factores
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+numero = int(input("¿De qué número desea los factores? "))
 
-# Proceso
-
+# Proceso 
+factores = [1]
+if numero % 2 == 0:
+    factores.append(2)
+if numero % 3 == 0:
+    factores.append(3)
+if numero % 5 == 0:
+    factores.append(5)
 
 # Salidas
-print(salida)
+print("Los factores de", numero, "son:", factores)
